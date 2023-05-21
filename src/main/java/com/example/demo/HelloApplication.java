4760dbbc5e7d02929fd672572bb85aa074e100da
Karl <oelschlager.karl@gmail.com>
2023-05-21 22:04:40 +0200

    Add Charts + Random numbers + Refresh

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -4,19 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scene1.fxml"));
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 
-
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("COOK MASTER DASHBOARD");
         stage.setScene(scene);
         stage.show();
+        
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/LineChartSample.java
+++ src/main/java/com/example/demo/LineChartSample.java
@@ -1,42 +1,89 @@
 package com.example.demo;
 
-import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Random;
 
 public class LineChartSample implements Initializable {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
     @FXML
     private BarChart<?,?> barChart; //fxid barchart
+    @FXML
+    private LineChart<?,?> lineChart; //fxid linechart
+    @FXML
+    private PieChart pieChart; //fxid piechart
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    	
+    	
             //defining a series
-            XYChart.Series series = new XYChart.Series();
-            series.setName("My portfolio");
+            XYChart.Series seriesBar = new XYChart.Series();
+            barChart.setTitle("Nombres de connexions d'utilisateurs différents");
             //populating the series with data
-            series.getData().add(new XYChart.Data("1", 23));
-            series.getData().add(new XYChart.Data("2", 14));
-            series.getData().add(new XYChart.Data("3", 15));
-            series.getData().add(new XYChart.Data("4", 24));
-            series.getData().add(new XYChart.Data("5", 34));
-            series.getData().add(new XYChart.Data("6", 36));
-            series.getData().add(new XYChart.Data("7", 22));
-            series.getData().add(new XYChart.Data("8", 45));
-            series.getData().add(new XYChart.Data("9", 43));
-            series.getData().add(new XYChart.Data("10", 17));
-            series.getData().add(new XYChart.Data("11", 29));
-            series.getData().add(new XYChart.Data("12", 25));
+            int k = 1;
+            while(k < 31) {
+            	int r = new Random().nextInt(90 + 1) + 10;
+            	seriesBar.getData().add(new XYChart.Data(Integer.toString(k), r));
+            	k++;
+            }
 
-            barChart.getData().addAll(series);
+            barChart.getData().addAll(seriesBar);
+            
+            XYChart.Series seriesLine = new XYChart.Series();
+            lineChart.setTitle("Chiffre d'affaire par mois");
+            //populating the series with data
+            int l = 1;
+            while(l < 13) {
+            	int r = new Random().nextInt(200 + 1) + 150;
+            	seriesLine.getData().add(new XYChart.Data(Integer.toString(l), r));
+            	l++;
+            }
+
+            lineChart.getData().addAll(seriesLine);
+            
+            int a = new Random().nextInt(200 + 1) + 80;
+            int b = new Random().nextInt(150 + 1) + 25;
+            int c = new Random().nextInt(100 + 1) + 5;
+            
+            ObservableList<PieChart.Data> pieChartData =
+                    FXCollections.observableArrayList(
+                    new PieChart.Data("Free Users", a),
+                    new PieChart.Data("Subscribers", b),
+                    new PieChart.Data("Premiums", c));
+            pieChart.setTitle("Répartitions de types de compte en %");
+            pieChart.setData(pieChartData);
+            
     }
+	
+	public void switchScene(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("scene1.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+    
+
+    
 }
